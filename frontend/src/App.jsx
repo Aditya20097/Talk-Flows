2ad3b38592a119d1a6a81b8cfdf5a9ff2f6c5888
Aditya20097 <raj.aditya10997@gmail.com>
2025-10-30 11:51:26 +0530

    onboarding started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,19 +16,26 @@ const App = () => {
   // tanstack query crash course
  const {isLoading , authUser} = useAuthUserHook()
 
-  
+  const isAuthenticated = Boolean(authUser)
+  const isOnboarded = authUser?.isOnboarded
+
+
   if(isLoading) return <PageLoader/>
 
   return (
     <div className=' h-screen' data-theme="night">
      <Routes>
-      <Route path='/' element ={ authUser ? < HomePage/> : <Navigate to='/login'/>}/>
-        <Route path='/signup' element ={!authUser ?  <SignUpPage/> : <Navigate to='/'/>}/>
-        <Route path='/login' element ={!authUser ?  <LoginPage/> : <Navigate to='/'/>}/>
-        <Route path='/onboarding' element ={ authUser ? <OnBoarding/> : <Navigate to='/login'/>}/>
-        <Route path='/notification' element ={authUser ?<NotificationPage/> :<Navigate to='/login'/>  }/>
-        <Route path='/chat' element ={authUser ? <ChatPage/> : <Navigate to='/login'/>}/>
-        <Route path='/call' element ={authUser ? <CallPage/>: <Navigate to='/login'/>}/>
+      <Route path='/' element ={  isAuthenticated && isOnboarded ? (
+        <HomePage/>
+      ): (
+        <Navigate to = {!isAuthenticated? "/login":"onboarding"}/>
+      )}/>
+        <Route path='/signup' element ={!isAuthenticated?  <SignUpPage/> : <Navigate to='/'/>}/>
+        <Route path='/login' element ={!isAuthenticated?  <LoginPage/> : <Navigate to='/'/>}/>
+        <Route path='/onboarding' element ={ isAuthenticated ? <OnBoarding/> : <Navigate to='/login'/>}/>
+        <Route path='/notification' element ={isAuthenticated ?<NotificationPage/> :<Navigate to='/login'/>  }/>
+        <Route path='/chat' element ={isAuthenticated ? <ChatPage/> : <Navigate to='/login'/>}/>
+        <Route path='/call' element ={isAuthenticated ? <CallPage/>: <Navigate to='/login'/>}/>
      </Routes>
      <Toaster/>
    </div>
--- frontend/src/pages/OnBoarding.jsx
+++ frontend/src/pages/OnBoarding.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useAuthUserHook from '../hooks/useAuthUserHook'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
 
 const OnBoarding = () => {
+  const {authUser} = useAuthUserHook()
+  const queryClient =  useQueryClient()
+
+  const [formState , setFormState] = useState({
+    fullName : authUser?.fullName || "",
+    bio : authUser?.bio || "",
+    nativeLanguage : authUser?.nativeLanguage || "",
+    learningLangugage:  authUser?.learningLangugage|| "",
+    location : authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  })
+
+  const {mutate:onboardingMutation , isPending} = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () => {
+      toast.success("profile onboarded successfully ")
+      queryClient.invalidateQueries({queryKey: ["authUser"]})
+    }
+  })
+
+
   return (
     <div>OnBoarding</div>
   )
